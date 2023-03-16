e18305f7d2953e27dabd9b256121ca9a9463c948
vikas <jnv2252@gmail.com>
2023-03-16 23:04:52 +0530

    foreign key with on delete cascade

---
+++ 15. Foreign key with On Delete Cascade.sql
@@ -0,0 +1,21 @@
+-- Foreign key with delete cascade
+-- use if we delete any value from column which is reference to another table
+-- all value of other table also delete automatic
+
+CREATE TABLE customers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50)
+);
+ 
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE,
+    amount DECIMAL(8 , 2 ),
+    customer_id INT,
+    FOREIGN KEY (customer_id)
+        REFERENCES customers (id)
+        ON DELETE CASCADE
+);
+
