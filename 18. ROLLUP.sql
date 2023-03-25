e8de1d7f3bb233b7c686dde98c60cca371c865a7
vikas <jnv2252@gmail.com>
2023-03-25 08:58:36 +0530

    Roll up in SQL

---
+++ 18. ROLLUP.sql
@@ -0,0 +1,22 @@
+-- ROLLUP
+
+SELECT 
+    title, AVG(rating)
+FROM
+    full_reviews
+GROUP BY title WITH ROLLUP;
+ 
+ 
+SELECT 
+    title, COUNT(rating)
+FROM
+    full_reviews
+GROUP BY title WITH ROLLUP;
+ 
+ 
+SELECT 
+    first_name, released_year, genre, AVG(rating)
+FROM
+    full_reviews
+GROUP BY released_year , genre , first_name WITH ROLLUP;
+
